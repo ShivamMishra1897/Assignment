d23fa12d8dcddf50cb74cd1dd121141213c41dc9
ShivamMishra1897 <113973330+ShivamMishra1897@users.noreply.github.com>
2022-10-13 23:16:15 +0530

    Add files via upload

---
+++ MODULE1.py
@@ -0,0 +1,25 @@
+'''
+What is Software?
+ANS-The general understanding of it would be  , it is a set of instruction or programs instructing a computer to do 
+specific tasks. It is generic term for computer programs 
+
+What are the types of Applications?
+ANS-there are different applications some are as below:-
+    Web brwosers
+    graphic software
+    database 
+    multimedia
+    educational 
+    word processors etc.
+
+What is programming
+ANS- Programming is writing code (a set of instructions) to be interpreted and executed by a computer or 
+another electronic device. These instructions can create programs, and tell applications how to operate
+or function. People who program are referred to as programmers and write their code using a programming language.
+Though it is much more broader skill.
+
+What is python?
+ANS- Python is an interpreted, object-oriented, high-level programming language.Python's simple,
+easy to learn syntax emphasizes readability.
+
+'''
