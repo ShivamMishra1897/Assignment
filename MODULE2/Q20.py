08245db013369d82a5b9be62a6935cda26402e89
ShivamMishra1897 <113973330+ShivamMishra1897@users.noreply.github.com>
2022-12-06 22:40:30 +0530

    Update Q20.py

--- MODULE2/Q20.py
+++ MODULE2/Q20.py
@@ -1,2 +1,12 @@
 #Write a Python function that takes a list of words and returns the length 
 #of the longest one. 
+
+def string(word):
+    wordlen = []
+    for n in word:
+        wordlen.append((len(n),n))
+    wordlen.sort()
+    return wordlen[-1][0], wordlen[-1][1]
+result = string(["Python", "apple", "lemonade"])
+print("\n Longest word: ",result[1])
+print("Length of the longest word: ",result[0])
