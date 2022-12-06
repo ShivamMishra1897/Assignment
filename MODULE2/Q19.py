6a5725fb52cf1fdc8dc917964c6d24325d84f7ce
ShivamMishra1897 <113973330+ShivamMishra1897@users.noreply.github.com>
2022-12-06 16:08:48 +0530

    Update Q19.py

--- MODULE2/Q19.py
+++ MODULE2/Q19.py
@@ -1,3 +1,13 @@
 #Write a Python program to find the first appearance of the substring 
 #'not' and 'poor' from a given string, if 'not' follows the 'poor', replace the 
 #whole 'not'...'poor' substring with 'good'. Return the resulting string. 
+
+def not_poor(str1):
+    str_not = str1.find('not')
+    str_bad = str1.find('poor')
+
+    if str_bad > str_not:
+     str1 = str1.replace(str1[str_not:(str_bad+4)],'good')
+
+    return str1
+print(not_poor('The lyrics is not that poor!'))
