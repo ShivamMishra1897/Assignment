5c9bad1de485f69427cbd329351eb941fc180f28
ShivamMishra1897 <113973330+ShivamMishra1897@users.noreply.github.com>
2023-02-05 23:42:29 +0530

    Add files via upload

---
+++ MODULE3/Q18.py
@@ -0,0 +1,4 @@
+#Write a Python program to create a tuple with different data types
+tuple = ("adani","budget","news",3.12,55)
+print(tuple)
+print(type(tuple))
---
+++ MODULE3/Q19.py
@@ -0,0 +1,5 @@
+#Write a Python program to create a tuple with numbers. 
+tuple = 5,6,8,85
+print(type(tuple))
+tuple = (5),
+print(type(tuple))
