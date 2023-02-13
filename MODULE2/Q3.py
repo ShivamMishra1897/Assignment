590d345e68428fa52ec36a363aac0399f34433ed
ShivamMishra1897 <113973330+ShivamMishra1897@users.noreply.github.com>
2023-02-13 12:43:27 +0530

    Update Q3.py

--- MODULE2/Q3.py
+++ MODULE2/Q3.py
@@ -1,9 +1,8 @@
 #Write a Python program to get the Fibonacci series of given range.
 #F(n) = F(n-1) + F(n-2), where n > 1  for nth number
-num = 10
 n1, n2 = 0, 1
 print("Fibonacci Series:", n1, n2, end=" ")
-for i in range(2, num):
+for i in range(2, 10):
     n3 = n1 + n2
     n1 = n2
     n2 = n3
